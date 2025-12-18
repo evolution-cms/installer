d55b52fbbd12ff128700e9c28c2370301573a97b
Serhii Korneliuk <info@seigerit.com>
2025-12-19 00:43:22 +0200

    [FIX] Unit Tests.

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EvolutionCMS\Installer\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testBasicFeature(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
